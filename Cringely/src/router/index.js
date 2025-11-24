00e5741b18350f5f13c6a152def805083929fc23
apjanusz <januszekaptacy2606@gmail.com>
2025-11-24 13:32:06 +0100

    small fixes

--- Cringely/src/router/index.js
+++ Cringely/src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue' 
-import MyTestsView from '../views/MyTestsView.vue' 
+import MyTestsView from '../views/MytestsView.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
